5690b4ebb8d99961eb5771a17a8c557ad5f9a4b7
zhantong1994 <zhantong1994@163.com>
2015-07-17 08:23:36 +0800

    增加获取音频文件下载链接目录

--- dl_list_temp.py
+++ dl_list_temp.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import os
+import codecs
 exist = []
 for root, dirs, files in os.walk("E:\TED"):
 	for name in files:
@@ -12,9 +13,17 @@ db = {
 cnx = mysql.connector.connect(
     user=db['user'], password=db['password'], database=db['database'])
 cursor = cnx.cursor()
-select='SELECT media_1500k FROM ted'
+#select='SELECT media_1500k FROM ted'
+#cursor.execute(select)
+#for item in cursor:
+#	if item[0]!='':
+#		if not item[0].split('/')[-1] in exist:
+#			print(item[0])
+select='SELECT audio FROM ted'
 cursor.execute(select)
-for item in cursor:
-	if item[0]!='':
-		if not item[0].split('/')[-1] in exist:
-			print(item[0])
+with codecs.open('audio_dl_list.txt','w','utf-8') as f:
+	for item in cursor:
+		if item[0]!='':
+			f.write(item[0]+'\n')
+cursor.close()
+cnx.close()
